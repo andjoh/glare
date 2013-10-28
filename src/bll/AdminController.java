b15c4b960e6df760009c46dcdd78ffdf2154f376
petterausterheim <petter.austerheim@gmail.com>
2013-10-28 20:07:10 +0100

    AdminController modified

--- src/bll/AdminController.java
+++ src/bll/AdminController.java
@@ -1,16 +1,37 @@
 package bll;
 
-public class AdminController 
-{
-	PictureController picCtrl;
+import java.util.*;
+
+import dal.*;
+
+public class AdminController {
+	private PictureController pictureController;
+	private DatabaseManager databaseManager;
 	
-	public AdminController(PictureController picCtrl) 
-	{
-		this.picCtrl = picCtrl;		
+	
+	public AdminController(PictureController pictureController, DatabaseManager databaseManager) {
+		this.pictureController = pictureController;
+		this.databaseManager   = databaseManager;
 	}
 	
-	public boolean searchPictures(String query, String source)
-	{
-		return picCtrl.searchPictures(query, source);
+	// Andreas: Kanskje vi kan bruke en metode som dekker på add og delete?
+	// Kanskje vi ikke trenger å tenke på om vi sletter eller legger til
+	// Vi bare oppdaterer lista enkelt og greit. Hva synes du?
+	public void updateHashtagBySource(String source, List<String> hashtag) {
+		databaseManager.updateHashtagBySource(source, hashtag);
 	}
+	
+//	public void addHashtagBySource(String source, List<String> hashtag) {
+//		databaseManager.addHashtagBySource(source, hashtag);
+//	}
+	
+	
+//	public void deleteHashtagBySource(String source, List<String> hashtag) {
+//		databaseManager.deleteHashtagBySource(source, hashtag);
+//	}	
+	
+	
+//	public void setAppropriateFlag(List<ThumbnailViewModel> thumbnailViewModel) {
+//		
+//	}
 }
