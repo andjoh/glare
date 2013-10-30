952adea4cac64a52e2de3f517e0cf4502ada9dd3
Marius Vasshus <marvas@lyse.net>
2013-10-30 12:22:01 +0100

    Fixed InstagramParser
    Fixed so no id is null

--- src/dal/InstagramParser.java
+++ src/dal/InstagramParser.java
@@ -16,41 +16,44 @@ public class InstagramParser {
 		JsonObject JObject = JParser.parse(reader).getAsJsonObject();
 		JsonArray jsonPictures = JObject.get("data").getAsJsonArray();
 
-		
+
 		for(JsonElement j: jsonPictures){
 			PictureData picture = new PictureData();
 
 			JsonObject jsonPicture = j.getAsJsonObject();
-		
-			if(jsonPicture.get("type").getAsString().equals("image")){
-				JsonElement images = jsonPicture.get("images");
 
-				//Gets url to Standard Resolution Picture
-				JsonElement imageStd = images.getAsJsonObject().get("standard_resolution");
-				String urlStd = imageStd.getAsJsonObject().get("url").getAsString();
-				picture.setUrlStd(urlStd);
+			//Checks if there is caption for picture
+			JsonElement caption = jsonPicture.get("caption");
 
-				//Gets url to Thumbnail Resolution Picture
-				JsonElement imageThumb = images.getAsJsonObject().get("thumbnail");
-				String urlThumb = imageThumb.getAsJsonObject().get("url").getAsString();
-				picture.setUrlThumb(urlThumb);
+			if(caption.isJsonObject()){
+				if(jsonPicture.get("type").getAsString().equals("image")){
 
-				//Sets removeFlag for picture
-				picture.setRemoveFlag(false);
-				
-				//Sets hashtag for picture
-				picture.setHashtag(searchTag);
-				
-				//Gets time and picture ID for the picture
-				JsonElement caption = jsonPicture.get("caption");
-				if(caption.isJsonObject()){
 					int time = caption.getAsJsonObject().get("created_time").getAsInt();
 					picture.setCreatedTime(time);
 
 					String picID = caption.getAsJsonObject().get("id").getAsString();
 					picture.setId(picID);
+
+					JsonElement images = jsonPicture.get("images");
+
+					//Gets url to Standard Resolution Picture
+					JsonElement imageStd = images.getAsJsonObject().get("standard_resolution");
+					String urlStd = imageStd.getAsJsonObject().get("url").getAsString();
+					picture.setUrlStd(urlStd);
+
+					//Gets url to Thumbnail Resolution Picture
+					JsonElement imageThumb = images.getAsJsonObject().get("thumbnail");
+					String urlThumb = imageThumb.getAsJsonObject().get("url").getAsString();
+					picture.setUrlThumb(urlThumb);
+
+					//Sets removeFlag for picture
+					picture.setRemoveFlag(false);
+
+					//Sets hashtag for picture
+					picture.setHashtag(searchTag);
+
+					pictures.add(picture);
 				}
-				pictures.add(picture);
 			}
 		}
 		return pictures;
