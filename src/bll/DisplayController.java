d56fa3d34ae55dadb0aa023a13a310e9efe89683
snuskelusken <kristinesvaboe@10.0.1.22>
2013-11-03 23:01:56 +0100

    Added Pseudocode in DisplayController

--- src/bll/DisplayController.java
+++ src/bll/DisplayController.java
@@ -2,4 +2,28 @@ package bll;
 
 public class DisplayController {
 
+	//getPictures from PictureController:
+	//public PictureViewModel[] GetPicturesToDisplay() {
+	//PictureGetter picGetter = new PictureGetter(currentContext); cC=IReader?
+	//Picture[] pictures = picGetter.GetPictures(); 
+	
+	//create list of PictureViewModel objects:
+	//List<PictureViewModel> pictureVms = new List<PictureViewModel>();
+	//for ( 100 newest elements of getPictures list)
+	//	PictureViewModel pVm = new PictureViewModel();
+	//	pVm.PictureUrl = picture.Url;
+	//	pVm.PictureId = picture.Id;
+	//	pictureVms.Add(pVm);
+	//currentPictureData = pictureVms - somewhere around here?
+	//return pictureVms;
+	
+	//method to return to GUI
+	//getPicturesToDisplay(viewMode String)
+	//if viewMode.equals(rand)
+	//	Collections.shuffle(pictureVms)
+	//	return pictureVms;
+	//else if viewMode.equals(seq)
+	//	Collections.sort(pictureVms) - Dunno if it is already sorted??
+	//	return pictureVms;
+	
 }
