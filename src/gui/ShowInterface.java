b00fd0779e63683b67df97759bef9c496d342524
andjoh <andjoh@users.noreply.github.com>
2013-11-08 19:24:17 +0100

    Update ShowInterface.java

--- src/gui/ShowInterface.java
+++ src/gui/ShowInterface.java
@@ -1,9 +1,12 @@
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class ShowInterface extends JFrame implements ActionListener {
+    /**
+ *
+ * @author ubuntu
+ */
 
     private final JButton settingsButton;
 
