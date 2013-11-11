7871dd2dfd658d9f3d7ba69eb220c4b3df53fe36
andjoh <andjoh@users.noreply.github.com>
2013-11-11 18:06:53 +0100

    Create LoginData.java
    LoginData - compares input login data with hardcoded data

---
+++ src/gui/LoginData.java
@@ -0,0 +1,12 @@
+
+ 
+public class LoginData {
+ 
+    public static boolean authenticate(String username, String password) {
+       
+        if (username.equals("root") && password.equals("root")) {
+            return true;
+        }
+        return false;
+    }
+}
