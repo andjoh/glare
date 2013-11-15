452ba1cc30f5ace0c6003ec9a102222f665b80a6
petterausterheim <petter.austerheim@gmail.com>
2013-11-15 15:30:22 +0100

    Created MainTest

---
+++ test/unit/MainTest.java
@@ -0,0 +1,60 @@
+package unit;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import dal.*;
+import bll.*;
+import gui.*;
+import resources.*;
+
+public class MainTest {
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) {
+
+		// Test using dummy DatabaseManager and dummy hashtag
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				DatabaseHandler dbHandler           = new DatabaseHandler();
+				DatabaseManagerDummy dbManagerDummy = new DatabaseManagerDummy(dbHandler);
+				PictureController picCtrl           = new PictureController(dbManagerDummy);
+
+				// Do some dummy stuff
+				List<String> sourcesDummy;
+				Set<String> hashtagsDummy;
+
+				// Set dummy sources
+				sourcesDummy = new ArrayList<String>();
+				sourcesDummy.add("instagram");
+				sourcesDummy.add("twitter");
+
+				// Set test data for source and hashtag
+				hashtagsDummy = new HashSet<String>();
+				hashtagsDummy.add("twittermotjavatesting");
+				hashtagsDummy.add("raskebriller");
+
+				// Set dummy sources and hashtag
+				dbManagerDummy.setSources(sourcesDummy);
+				dbManagerDummy.setHashtags(hashtagsDummy);
+
+				// Run test
+				picCtrl.searchPictureData();
+				picCtrl.processPictureData();
+				DisplayController dc = new DisplayController(picCtrl);
+				try {
+					ShowInterface showInterface = new ShowInterface(dc);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
