206b5b74ec0dd232bedff3a39f8fa5d5678c50b6
solfuglen <simen.sollie+github@gmail.com>
2013-11-15 14:53:18 +0100

    modified displaycontroller

--- src/bll/DisplayController.java
+++ src/bll/DisplayController.java
@@ -18,15 +18,18 @@ import dal.PictureData;
  */
 
 public class DisplayController {
-	
+
 	/**
 	 * Manage list with pictures
 	 */
 
+	private ArrayList<BufferedImage> po;
+	private List<PictureData> sortedPictureList;
 	private PictureController pictureController;
 	private BufferedImage currentPic;
 	private int count = 0;
-	
+	private final int MAX_SIZE = 100;
+
 	public DisplayController(PictureController pictureController){
 		this.pictureController = pictureController;
 	}
@@ -34,42 +37,51 @@ public class DisplayController {
 	public BufferedImage getCurrentPicture(boolean random) throws IOException{
 		System.out.println("PictureController: getCurrentPicture");
 
-		ArrayList<BufferedImage> po = new ArrayList<BufferedImage>();
-		List<PictureData> sortedPictureList = pictureController.getPictureDataToDisplay();
-		int i = 1;
-		for (PictureData p : sortedPictureList){
-			po.add(getBufImage(p));
-			i++;
-			if (i > 100){ //hva skjer hvis listen ikke har 100 objekt?
-				break;
+		if (count == 0) {
+			po = new ArrayList<BufferedImage>();
+			sortedPictureList = pictureController.getPictureDataToDisplay();
+
+			int i = 1;
+			for (PictureData p : sortedPictureList){
+				po.add(getBufImage(p));
+				System.out.println(p.getUrlStd());
+				System.out.println(p.getId());
+				i++;
+				if (i > MAX_SIZE){ 
+					break;
+				}
 			}
 		}
+		
+		
 		shufflePictures(random, po);
 		currentPicture(po);
-		
+
 		return currentPic;
 	}
+	
+	private void currentPicture(ArrayList<BufferedImage> po){
+		if (count < po.size()-1){
+			currentPic = po.get(count);
+			count++;
+		} else {
+			currentPic = po.get(count);
+			count = 0;
+		}
+	}
+	
 	private BufferedImage getBufImage(PictureData p) throws IOException{
-//		URL testUrl = new URL("http://pbs.twimg.com/media/BXrietbIgAAiroP.jpg");
+		//		URL testUrl = new URL("http://pbs.twimg.com/media/BXrietbIgAAiroP.jpg");
 		URL imageUrl = new URL(p.getUrlStd());
 		InputStream in = imageUrl.openStream();
 		BufferedImage image = ImageIO.read(in);
 		in.close();
 		return image;
 	}
+	
 	private void shufflePictures(boolean random, ArrayList<BufferedImage> po){
 		if (random = true){
 			Collections.shuffle(po);
-		}
-	}
-	private void currentPicture(ArrayList<BufferedImage> po){
-		if (count < 100){
-			currentPic = po.get(count);
-			count++;
-		} else {
-			count = 0;
-			currentPic = po.get(count);
-			count++;
 		}
 	}
 }
