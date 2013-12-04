c8158d60b97a90c7dc1be2b23a02d09766138a53
Andreas Bjerga <andreas.bjerga@gmail.com>
2013-12-04 11:54:25 +0100

    Cleaned up DB test-cases

---
+++ test/unit/HibernateHashtagTests.java
@@ -0,0 +1,38 @@
+package unit;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import dal.DatabaseHandler;
+import dal.Hashtag;
+
+public class HibernateHashtagTests {
+	@Test
+	public void AddHashtagToDB_ReturnListOfHashtagsWhenHashtagIsRemovedFromDb() {
+		Hashtag h1 = new Hashtag("test");
+		
+		DatabaseHandler.addHashtagToDB(h1);
+		
+		DatabaseHandler.removeHashtagFromDB(h1.getHashtag());
+		
+		ArrayList<String> hashtags = (ArrayList<String>) DatabaseHandler.listOfHashtagsFromDB();
+		
+		assertThat(hashtags.size(), is(0));
+	}
+	
+	@Test
+	public void AddExistingHashtagsToDB_ReturnListWithHashtagsFromDB_CheckThatSizeIsOne() {
+		Hashtag hash = new Hashtag("winter");
+		
+		DatabaseHandler.addHashtagToDB(hash);
+		DatabaseHandler.addHashtagToDB(hash);
+		
+		ArrayList<String> hashtags = (ArrayList<String>) DatabaseHandler.listOfHashtagsFromDB();
+		
+		assertThat(hashtags.size(), is(1));
+	}
+}
