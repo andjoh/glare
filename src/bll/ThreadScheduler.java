97b8022046e66735662019e708f41638edf57cf1
petterausterheim <petter.austerheim@gmail.com>
2013-12-04 16:29:49 +0100

    ThreadScheduler: Moved sleep after getNewPictures

--- src/bll/ThreadScheduler.java
+++ src/bll/ThreadScheduler.java
@@ -16,8 +16,9 @@ public class ThreadScheduler implements Runnable{
 	public void run() {
 		while(true){
 			try {
-				Thread.sleep(120000);
+				System.out.println("HENTER NYE BILDER!!!!!!!!!!!!!!!!!!!!!!!!");
 				pc.getNewPictureData();
+				Thread.sleep(60000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
