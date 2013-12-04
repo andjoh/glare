b1facb12fde8727ae42deee530f5938b3a4241ae
petterausterheim <petter.austerheim@gmail.com>
2013-12-04 12:49:34 +0100

    MainTestFromDb: Modified

--- test/unit/MainTestFromDb.java
+++ test/unit/MainTestFromDb.java
@@ -1,6 +1,18 @@
 package unit;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import resources.DatabaseManagerDummy;
+import dal.DatabaseHandler;
+import dal.IReader;
 import bll.*;
 import glare.*;
 import gui.*;
@@ -14,16 +26,37 @@ public class MainTestFromDb {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) {
-
+		try {
+			for (UIManager.LookAndFeelInfo info : UIManager
+					.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(SettingsFrame.class.getName())
+					.log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(SettingsFrame.class.getName())
+					.log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(SettingsFrame.class.getName())
+					.log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(SettingsFrame.class.getName())
+					.log(java.util.logging.Level.SEVERE, null, ex);
+		}
 		// Test using dummy DatabaseManager and dummy hashtag
-		java.awt.EventQueue.invokeLater(new Runnable() {
+		SwingUtilities.
+		invokeLater(new Runnable() {
 			@Override
 			public void run() {
-								
-				ViewController vc = (ViewController) ClassFactory.getBeanByName("viewController");
+				ViewController vc  = (ViewController) ClassFactory.getBeanByName("viewController");
+				ShowInterface showInterface =null;
 
 				try {
-					ShowInterface showInterface = new ShowInterface(vc);
+					showInterface = new ShowInterface(vc);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
