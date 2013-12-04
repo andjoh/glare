7d4582b794c29affdacd8df1a91ccab31dc0574c
Andreas Bjerga <andreas.bjerga@gmail.com>
2013-12-04 15:43:33 +0100

    Changes to make thread run forever

--- src/bll/ThreadScheduler.java
+++ src/bll/ThreadScheduler.java
@@ -7,20 +7,22 @@ package bll;
  */
 public class ThreadScheduler implements Runnable{
 	private PictureController pc;
-	
+
 	public ThreadScheduler(){
 		pc = (PictureController) glare.ClassFactory.getBeanByName("pictureController");
 	}
 
 	@Override
 	public void run() {
-		try {
-			Thread.sleep(120000);
-			pc.getNewPictureData();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
+		while(true){
+			try {
+				Thread.sleep(120000);
+				pc.getNewPictureData();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		}
 	}
 
 }
