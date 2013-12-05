bf5cea4d0e1668ede511578ed4057aa10c5a0eb7
petterausterheim <petter.austerheim@gmail.com>
2013-12-05 14:42:43 +0100

    TwitterReaderDummy and InstagramReaderDummy: changed urls

--- test/resources/InstagramReaderDummy.java
+++ test/resources/InstagramReaderDummy.java
@@ -12,32 +12,32 @@ public class InstagramReaderDummy implements IReader{
 		PictureData p = new PictureData();
 		p.setId("picID 1");
 		p.setCreatedTime(1);
-		p.setUrlStd("http://distilleryimage3.s3.amazonaws.com/ec234b644b9c11e39c6812ce5dbdd1cb_8.jpg");
-		p.setUrlThumb("http://distilleryimage3.s3.amazonaws.com/ef1236282adb11e3a9b322000a9e5afc_5.jpg");
+		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
+		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
 		pictureData.add(p);
 		
 		p = new PictureData();
 		p.setId("picID 2");
 		p.setCreatedTime(2);
-		p.setUrlStd("http://distilleryimage3.s3.amazonaws.com/f2275bfc4a1d11e39a131216ecc38082_8.jpg");
-		p.setUrlThumb("http://images.ak.instagram.com/profiles/profile_186344368_75sq_1380238572.jpg");
+		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
+		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
 		pictureData.add(p);
 		
 		p = new PictureData();
 		p.setId("picID 4");
 		p.setCreatedTime(4);
-		p.setUrlStd("http://distilleryimage4.s3.amazonaws.com/95583912472711e391f8122b2cbdf00c_8.jpg");
-		p.setUrlThumb("http://images.ak.instagram.com/profiles/profile_186344368_75sq_1380238572.jpg");
+		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
+		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
 		pictureData.add(p);
 
 		p = new PictureData();
 		p.setId("picID 2");
 		p.setCreatedTime(2);
-		p.setUrlStd("http://distilleryimage3.s3.amazonaws.com/f2275bfc4a1d11e39a131216ecc38082_8.jpg");
-		p.setUrlThumb("http://images.ak.instagram.com/profiles/profile_186344368_75sq_1380238572.jpg");
+		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
+		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
 		pictureData.add(p);
 		
--- test/resources/TwitterReaderDummy.java
+++ test/resources/TwitterReaderDummy.java
@@ -14,32 +14,32 @@ public class TwitterReaderDummy implements IReader{
 		PictureData p = new PictureData();
 		p.setId("picID 1");
 		p.setCreatedTime(1);
-		p.setUrlStd("http://distilleryimage3.s3.amazonaws.com/ec234b644b9c11e39c6812ce5dbdd1cb_8.jpg");
-		p.setUrlThumb("http://distilleryimage3.s3.amazonaws.com/ef1236282adb11e3a9b322000a9e5afc_5.jpg");
+		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
+		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
 		pictureData.add(p);
 		
 		p = new PictureData();
 		p.setId("picID 7");
 		p.setCreatedTime(7);
-		p.setUrlStd("http://pbs.twimg.com/media/BY7y8IYIcAAW5_w.jpg:large");
-		p.setUrlThumb("http://images.ak.instagram.com/profiles/profile_186344368_75sq_1380238572.jpg");
+		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
+		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
 		pictureData.add(p);
 		
 		p = new PictureData();
 		p.setId("picID 8");
 		p.setCreatedTime(8);
-		p.setUrlStd("http://distilleryimage9.s3.amazonaws.com/76f1930434d211e393fe22000a1fcb64_8.jpg");
-		p.setUrlThumb("http://images.ak.instagram.com/profiles/profile_186344368_75sq_1380238572.jpg");
+		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
+		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
 		pictureData.add(p);
 
 		p = new PictureData();
 		p.setId("picID 7");
 		p.setCreatedTime(7);
-		p.setUrlStd("http://pbs.twimg.com/media/BY7y8IYIcAAW5_w.jpg:large");
-		p.setUrlThumb("http://images.ak.instagram.com/profiles/profile_186344368_75sq_1380238572.jpg");
+		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
+		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
 		pictureData.add(p);
 		
