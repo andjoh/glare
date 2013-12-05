bef3a9dda66e7e22edd55bcdeb9488a818a1b6da
petterausterheim <petter.austerheim@gmail.com>
2013-12-05 20:31:19 +0100

    Inst and twit dummy classes modified

--- test/resources/TwitterReaderDummy.java
+++ test/resources/TwitterReaderDummy.java
@@ -20,25 +20,17 @@ public class TwitterReaderDummy implements IReader{
 		pictureData.add(p);
 		
 		p = new PictureData();
-		p.setId("picID 7");
-		p.setCreatedTime(7);
+		p.setId("picID 2");
+		p.setCreatedTime(2);
 		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
-		pictureData.add(p);
+		pictureData.add(p);		
 		
 		p = new PictureData();
-		p.setId("picID 8");
-		p.setCreatedTime(8);
-		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
-		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
-		p.addHashtag(new Hashtag(searchTag));
-		pictureData.add(p);
-
-		p = new PictureData();
 		p.setId("picID 7");
 		p.setCreatedTime(7);
-		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
+		p.setUrlStd("http://distilleryimage5.s3.amazonaws.com/22abecf65dd911e3ba4412c54af81bed_5.jpg");
 		p.setUrlThumb("http://distilleryimage5.s3.amazonaws.com/b50b6e1097bd11e2934722000a9f3cae_7.jpg");
 		p.addHashtag(new Hashtag(searchTag));
 		pictureData.add(p);
