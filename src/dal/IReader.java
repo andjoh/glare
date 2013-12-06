2c8e61e3530bd640610a6e110874938cc31c92c7
solfuglen <simen.sollie+github@gmail.com>
2013-12-06 14:20:27 +0100

    Added comments in IReader.java

--- src/dal/IReader.java
+++ src/dal/IReader.java
@@ -3,7 +3,12 @@ package dal;
 import java.util.List;
 
 public interface IReader {
-	
+	/**
+	 * Is used by TwitterReader.java & InstagramReader
+	 * @param searchTag
+	 * @return
+	 * @throws Exception
+	 */
 	public List<PictureData> getPictures(String searchTag) throws Exception;
 	
 }
